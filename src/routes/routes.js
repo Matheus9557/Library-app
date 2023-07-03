8f1785902218e0baf42126c8ad0e3edd67977965
Matheus Gomes <79982182+Matheus9557@users.noreply.github.com>
2023-07-03 14:55:23 -0300

    Atualizando routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,49 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
-const { isAuthenticated, isAPIAuthenticated } = require('../middleware');
+const { errors } = require('celebrate');
+const middleware = require('../middleware');
 const UserController = require('../controllers/userController');
 const UserAPIController = require('../controllers/userAPIController');
 const BookAPIController = require('../controllers/booksAPIController');
 const BookController = require('../controllers/booksController');
 const LoanController = require('../controllers/loanController');
 
-router.get('/', (req, res) => res.redirect('/books/index'));
+// Middleware de erros do Celebrate
+router.use(errors());
 
-router.post('/signup', UserAPIController.store);
-router.post('/signin', UserAPIController.authenticate);
+// Middleware para inicializar variáveis locais
+router.use(middleware.initLocals);
 
+// Rotas para autenticação de usuários
+router.post('/users/signup', UserAPIController.store);
+router.post('/users/signin', UserAPIController.authenticate);
+router.get('/users/signup', middleware.isAuthenticated, UserController.create);
+router.get('/users/signin', middleware.isAuthenticated, UserController.signin);
+router.get('/users/signout', middleware.isAuthenticated, UserController.signout);
+router.post('/users/signup', middleware.isAuthenticated, UserController.store);
+router.post('/users/signin', middleware.isAuthenticated, UserController.authenticate);
 
+// Rotas para API de livros
+router.get('/api/v1/books', middleware.isAPIAuthenticated, BookAPIController.readAll);
+router.get('/api/v1/books/:id', middleware.isAPIAuthenticated, BookAPIController.readById);
+router.post('/api/v1/books', middleware.isAPIAuthenticated, BookAPIController.create);
+router.put('/api/v1/books/:id', middleware.isAPIAuthenticated, BookAPIController.update);
+router.delete('/api/v1/books/:id', middleware.isAPIAuthenticated, BookAPIController.destroy);
 
+// Rotas para livros
+router.get('/books', middleware.isAuthenticated, BookController.index);
+router.get('/books/create', middleware.isAuthenticated, BookController.getCreateForm);
+router.post('/books/create', middleware.isAuthenticated, BookController.create);
+router.get('/books/:id/update', middleware.isAuthenticated, BookController.getUpdateForm);
+router.post('/books/:id/update', middleware.isAuthenticated, BookController.update);
+router.get('/books/:id/delete', middleware.isAuthenticated, BookController.getDeleteForm);
+router.post('/books/:id/delete', middleware.isAuthenticated, BookController.destroy);
 
-router.get('/signup',isAuthenticated,UserController.create);
-router.get('/signin',isAuthenticated,UserController.signin);
-router.get('/signout',isAuthenticated,UserController.signout);
-router.post('/signup',isAuthenticated,UserController.store);
-router.post('/signin',isAuthenticated,UserController.authenticate);
+// Rotas para API de empréstimos
+router.get('/api/v1/loans', middleware.isAPIAuthenticated, LoanController.getAllLoans);
+router.get('/api/v1/loans/:id', middleware.isAPIAuthenticated, LoanController.getLoanById);
+router.post('/api/v1/loans', middleware.isAPIAuthenticated, LoanController.createLoan);
+router.put('/api/v1/loans/:id', middleware.isAPIAuthenticated, LoanController.updateLoan);
+router.delete('/api/v1/loans/:id', middleware.isAPIAuthenticated, LoanController.deleteLoan);
 
-
-router.get('/books', isAPIAuthenticated, BookAPIController.readAll);
-router.get('/books/:id', isAPIAuthenticated, BookAPIController.readById);
-router.post('/books', isAPIAuthenticated, BookAPIController.create);
-router.put('/books/:id', isAPIAuthenticated, BookAPIController.update);
-router.delete('/books/:id', isAPIAuthenticated, BookAPIController.destroy);
-
-
-router.get('/books', isAuthenticated, BookController.index);
-router.get('/books/create', isAuthenticated, BookController.getCreateForm);
-router.post('/books/create', isAuthenticated, BookController.create);
-router.get('/books/:id/update', isAuthenticated, BookController.getUpdateForm);
-router.post('/books/:id/update', isAuthenticated, BookController.update);
-router.get('/books/:id/delete', isAuthenticated, BookController.getDeleteForm);
-router.post('/books/:id/delete', isAuthenticated, BookController.destroy);
-
-
-router.get('/loans', isAPIAuthenticated, LoanController.getAllLoans);
-router.get('/loans/:id', isAPIAuthenticated,  LoanController.getLoanById);
-router.post('/loans', isAPIAuthenticated,  LoanController.createLoan);
-router.put('/loans/:id', isAPIAuthenticated,  LoanController.updateLoan);
-router.delete('/loans/:id', isAPIAuthenticated,  LoanController.deleteLoan);
-
+// Rota raiz redireciona para /foods/index
+router.get('/', (req, res) => res.redirect('/foods/index'));
 
 module.exports = router;
